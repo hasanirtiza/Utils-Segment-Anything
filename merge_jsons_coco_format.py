23ea99261bd85ba8a0cae794750c8576d09b7d3a
Irtiza Hasan <ihasan085@gmail.com>
2023-08-21 18:38:26 +0300

    bug fixes

--- merge_jsons_coco_format.py
+++ merge_jsons_coco_format.py
@@ -2,72 +2,112 @@ import json
 import os
 import sys
 import pickle
+from pycocotools import mask as mask_utils
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+from itertools import groupby
 # Define the directory containing JSON files
 json_directory = "./sample_json/"
 
 
-class ProgressFile:
-    def __init__(self, file, callback=None):
-        self.file = file
-        self.callback = callback
-        self.bytes_written = 0
+def binary_mask_to_rle(binary_mask):
+    rle = {'counts': [], 'size': list(binary_mask.shape)}
+    counts = rle.get('counts')
+    for i, (value, elements) in enumerate(groupby(binary_mask.ravel(order='F'))):
+        if i == 0 and value == 1:
+            counts.append(0)
+        counts.append(len(list(elements)))
+    return rle
 
-    def write(self, data):
-        self.bytes_written += len(data)
-        if self.callback:
-            self.callback(self.bytes_written)
-        self.file.write(data)
 
-    def __getattr__(self, attr):
-        return getattr(self.file, attr)
+def is_bbox_valid(bbox, image_width, image_height):
+    x, y, w, h = bbox
 
+    # Check if all values are positive
+    if x < 0 or y < 0 or w <= 0 or h <= 0:
+        return False
+
+    # Check if the bounding box is within the image dimensions
+    if x + w > image_width or y + h > image_height:
+        return False
+
+    return True
 # Example callback function
 def progress_callback(bytes_written):
     print(f"Bytes written: {bytes_written}")
 
-combined_data = {
+# Initialize the COCO annotation dictionary
+coco_annotation = {
+    "info": {
+        "description": "Object detection annotations in COCO format",
+        "version": "1.0",
+        "year": 2023,
+        "date_created": "2023-06-26"
+    },
+    "licenses": [],
     "images": [],
     "annotations": [],
-     "categories": []
-}
-category = {
-    "id": 1,
-    "name": "object",
-    "supercategory": "object"
+    "categories": []
 }
 
+# Define the category
+category = {
+    "id": 1,
+    "name": "tower",
+    "supercategory": "tower"
+}
+coco_annotation["categories"].append(category)
 total_files = len(os.listdir(json_directory))
-ann_counter = 1
+ann_counter = 0
 for filename in os.listdir(json_directory):
     if filename.endswith(".json"):
         with open(os.path.join(json_directory, filename), "r") as json_file:
             data = json.load(json_file)
-            data["image"]["id"] = data["image"]["image_id"]
-            combined_data["images"].append(data["image"])
-            for anno in data["annotations"]:
-                anno["image_id"] = data["image"]["image_id"]
-                anno["category_id"] = 1
-                anno["iscrowd"] = 0
-                anno["id"] = ann_counter
-                ann_counter += 1
-                combined_data["annotations"].append(anno)
 
+
+        image_entry = {
+        "id": data["image"]["image_id"],
+        "file_name": data["image"]["file_name"],
+        "height": data["image"]["height"],
+        "width": data["image"]["width"]
+        }
+        coco_annotation["images"].append(image_entry)
+        im_name = json_directory + data["image"]["file_name"]
+        im = cv2.imread(im_name)
+        print("total anos", len(data["annotations"]))
+        for counter,anno in enumerate(data["annotations"]):
+
+            #mask = mask_utils.decode(anno["segmentation"])
+            x, y, w, h = np.array(anno["bbox"]).astype(np.int32)
+
+            #rle_mask = binary_mask_to_rle(mask)
+            annotation_entry = {
+            "id": ann_counter,
+            "image_id": data["image"]["image_id"],
+            "category_id": category["id"],
+            "bbox": anno["bbox"],
+            "area": anno["area"],
+            "segmentation": anno["segmentation"],
+            "iscrowd": 0
+            }
+            coco_annotation["annotations"].append(annotation_entry)
+            ann_counter += 1
+                    
+                #cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 0))
+
+
+            #combined_data["annotations"].extend(data["annotations"])
 
     total_files = total_files - 1
     print("Remaining files:", total_files)
-    combined_data["categories"].append(category)
+
 
 # Define the output JSON file path
-output_json_path = "val_sam_set.json"
+output_json_path = "val_json2.json"
 
-dict_size_bytes = sys.getsizeof(pickle.dumps(combined_data, protocol=pickle.HIGHEST_PROTOCOL))
-
-# Convert bytes to gigabytes
-dict_size_gb = dict_size_bytes / (1024 ** 3)
-print(f"Estimated dictionary size: {dict_size_gb:.6f} GB")
 
 # Write the combined data to the output JSON file
 with open(output_json_path, "w") as output_json_file:
-    progress_file = ProgressFile(output_json_file, callback=progress_callback)
-    json.dump(combined_data, output_json_file, indent=4)
+    json.dump(coco_annotation, output_json_file)
 
